579fd55ed0c0b11c2705690fa9ae4cb029b8bf61
yue liang <81293063+inumakie@users.noreply.github.com>
2022-11-12 01:06:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,65 +7,26 @@ function call(){
         <div>
             <h2>masticando celulares</h2>
             <p style="margin-bottom: 40px;">
-                Al final de mi vida, luego de sentirme desvanecer en la oscuridad pura,
-                desperté en una habitación desconocida. 
-                Se trataba de un lugar muy iluminado, pero sin fuentes de luz a la vista.
-                no ví ninguna lámpara o pantalla. la luz parecía emanar de las paredes. 
-                El piso, las paredes y el techo eran completamente blancos pero con un
-                Tenue Tono Violeta, casi impercepible. Sobre el techo se proyectaban
-                los patrones que produce la luz al reflejarse sobre la superficie del agua, 
-                lineas entrecortadas que dibujan las olas con una luz cristalina y bordes de arcoiris,
-                pero no había agua a la vista. la habitación parecía muy pequeña, pero conforme empece
-                a caminar y moverme, cambiaba su tamaño. cuanto más me alejaba
-                del punto donde desperté, más grande se volvía la habitación. cuando volvía hacia atras,
-                la habitación volvía a encogerse. no podia tocar las paredes. eran holográficas.
-                no podia cruzarlas, no podía hacer nada.
-                al caminar mi cuerpo se sentía raro. todo parecía un juego muy realista, en el aspecto que
-                todos mis sentidos estaban ahí, pero parecía que hubiese un espacio entre el estímulo
-                y la reacción. Como si estuviese anestesiado.
+            Al final de mi vida, luego de sentirme desvanecer en la oscuridad pura, desperté en una habitación desconocida. Se trataba de un lugar muy iluminado, pero no había fuentes de luz a la vista, -ninguna lámpara o pantalla- sino que la luz parecía emanar de las paredes. El piso, las paredes y el techo eran completamente blancos pero con un Tenue Tono Violeta, casi imperceptible. Sobre el techo se proyectaban los patrones que produce la luz al reflejarse sobre la superficie del agua, líneas entrecortadas que dibujan las olas con una luz cristalina y bordes de arcoíris, pero no había agua a la vista. La habitación parecía muy pequeña, pero conforme empece a caminar y moverme, cambiaba su tamaño. Cuanto más me alejaba del punto donde desperté, más grande se volvía la habitación. Cuando volvía hacia atrás, la habitación volvía a encogerse. No podía tocar las paredes. Eran holográficas. No podía cruzarlas, no podía hacer nada. Al caminar mi cuerpo se sentía raro. Todo parecía un juego muy realista, en el sentido que todos mis sentidos estaban ahí, pero parecía que hubiese un espacio entre el estímulo y la reacción. Como si estuviese anestesiado.
             </p>
 
             <h2>UUNO</h2>
             <p style="margin-bottom: 40px;">
-            Aparece una mancha gigante en la atmósfera. Una formación de luces con forma de algas marinas levitan sobre el polo sur del planeta. Todas las personas sufren una alteración espontánea del sistema nervioso, que consiste en una momentanea falta de sensibilidad y un instinto por electrocutarse. Por un periodo de tiempo, nada duele ni daña y todos sienten sed de electricidad. Billones de personas dejan de lado lo que sea que estaban haciendo y comienzan a romper cables de todo tipo para exponer los filamentos conductores y saborear una dosis fuerte de electrones. Algunas personas muerden lamparas encendidas. Hay suicidios masivos en las estaciones de subte. Todo el ambiente se llena de flashes de luz blanca, humo y vapor. La mancha en la atmósfera desaparece y las luces con forma de algas salen disparadas a otro lugar del cosmos.
-            </p>
+            Aparece una mancha gigante en la atmósfera. Una formación de luces con forma de algas marinas levitan sobre el polo sur del planeta. Todas las personas sufren una alteración espontánea del sistema nervioso, que consiste en una momentánea falta de sensibilidad y un instinto por electrocutarse. Por un periodo de tiempo, nada duele ni daña y todos sienten sed de electricidad. Billones de personas dejan de lado lo que sea que estaban haciendo y comienzan a romper cables de todo tipo para exponer los filamentos conductores y saborear una dosis fuerte de electrones. Algunas personas muerden lámparas encendidas. Hay suicidios masivos en las estaciones de subte. Todo el ambiente se llena de flashes de luz blanca, humo y vapor. La mancha en la atmósfera desaparece y las luces con forma de algas salen disparadas a otro lugar del cosmos.            </p>
 
             <h2>cero</h2>
             <p style="margin-bottom: 40px;">
-            la fantasia es lo más real que existe, "la realidad" es un algoritmo al que sometemos el cuerpo para vivir,
-            pero para que vas a someter el cuerpo a la vida si no te vas a permitir florecer tu cerebro?
-            darle colores y sonidos y comerlo como una sandia con el agua chorreando por las comisuras de los labios.
-            ecstasis.
+            La fantasía es lo más real que existe, "la realidad" es un algoritmo al que sometemos el cuerpo para vivir, pero, ¿para qué vas a someter el cuerpo a la vida si no te vas a permitir florecer tu cerebro? Darle colores y sonidos y comerlo como una sandia con el agua chorreando por las comisuras de los labios. Éctasis.
             </p>
-
 
             <h2>encuentro</h2>
             <p style="margin-bottom: 40px;">
-
-            En un momento sentí que un cuervo gigante se apoyaba contra mi cuerpo.
-            Era un cuervo de una altura exactamente igual a la mía.
-            Podía sentir la suavidad de las alas y las plumas tocando mis brazos.
-            A veces apoyaba su pico en la punta de mi nariz y entonces sentía como crecían
-            ramas de árbol dentro de mi cuerpo. Me hacía volverme consciente de mi propia respiración.
-            El cuervo telepáticamente transmitía tranquilidad. Se alimentaba de
-            mi ansiedad, miedo y dolor. El animal era muy sabio, en una ocasión me dijo que
-            necesitaba mi ansiedad como una planta necesita mi dioxido de carbono.
-            me dijo eso cuando pensé que el hecho de que se alimente de emociones
-            negativas podría ser perjudicial para su salud. o darle un leve dolor de estómago.
-            "Entendes tan poco" dijo.            
+            En un momento sentí que un cuervo gigante se apoyaba contra mi cuerpo. Era un cuervo de una altura exactamente igual a la mía. Podía sentir la suavidad de las alas y las plumas rozando mis brazos. A veces apoyaba su pico en la punta de mi nariz y entonces sentía como crecían ramas de árbol dentro de mi cuerpo. Me hacía volverme consciente de mi propia respiración. El cuervo telepáticamente transmitía tranquilidad. Se alimentaba de mi ansiedad, miedo y dolor. El animal era muy sabio, en una ocasión me dijo que necesitaba mi ansiedad como una planta necesita mi dióxido de carbono. Me dijo eso cuando pensé que el hecho de que se alimente de emociones negativas podría ser perjudicial para su salud. O darle un leve dolor de estómago. "Entendés tan poco" dijo. 
             </p>
 
             <h2>invoka</h2>
             <p style="margin-bottom: 40px;">
-            Durante mis lecturas aleatorias de material esotérico pesado en las profundidades
-            de la madrugada, mi mente se alinea con otro plano y soy susceptible a ver 
-            cosas feas. demonios.
-            solía quedarme petrificado por periodos largos de tiempo.
-            el miedo me paralizaba.
-            pero un día el miedo me canso o aburrio y entendi que
-            si el diablo quiere lastimarte, ya lo habría hecho.
-            y que la realidad es que no puede hacer más que provocarte miedo.
-            no es su culpa tener hambre de miedo ajeno en vez de hambre de un sanguchito de miga con coca y papas fritas.
+            Durante mis lecturas aleatorias de material esotérico pesado en las profundidades de la madrugada, mi mente se alinea con otro plano y soy susceptible a ver cosas feas. Demonios. solía quedarme petrificado por periodos largos de tiempo. El miedo me paralizaba. Pero un día el miedo me canso o aburrió y entendí que si el diablo quiere lastimarte, ya lo habría hecho. Y que la realidad es que no puede hacer más que provocarte miedo. No es su culpa tener hambre de miedo ajeno en vez de hambre de un sanguchito de miga con coca y papas fritas.
             </p>
 
             <h2>lain</h2>
@@ -80,18 +41,12 @@ function call(){
 
             <h2>anubis</h2>
             <p style="margin-bottom: 40px;">
-            antes de poder procesar algún otro pensamiento, en una de las paredes comenzo a aparecer un borrón oscuro del tamaño de una puerta. Era como un agujero negro pero sin un contorno sólido, sino que la oscuridad se desvanecia en un degradado. En segundos, el abismo al otro lado fue tomando forma de a poco, como una foto instantánea revelandose. Claramente era un agujero que guiaba hacia fuera de la habitación. Me acerque hacia el portal y pude ver claramente que a pocos pasos del otro lado había un largo pasillo de baldosas de cristal rojo, rodeadas de vacío puro. No dude ni un segundo en salir de la habitación y pararme sobre las baldosas. La transición del piso de la habitación al piso de baldosas me dio tranquilidad, porque apenas pise el suelo del pasillo, note que mis sentidos volvian a su lugar correcto. Sentia mi respiración y el frío de las baldosas. El pasillo se extendía como cien metros, en plena oscuridad y conforme avanzaba, las baldosas detrás mio se desvanecian. Al comienzo me asuste porque pense que se estaban cayendo, pero luego me di cuenta que si me detenía en un lugar, las baldosas dejaban de desvanecerse. Era como si hubiera un foco de luz siguiendome al caminar. ¿Son rojas las baldosas o quizás son de color cristal y hay una luz de tono rojizo encima mio? Sobre mi no habia ningún resplandor que confirmase mis pensamientos, pero al mirar hacia arriba un rato, me di cuenta que se iban formando pequeños puntos brillantes, miles de estrellas. cuanto más me quedaba mirando al vacío más detalles aparecían, de la misma manera que los ojos solo ven un par de estrellas cuando salimos de una habitación iluminada hacia afuera y miramos el cielo nocturno y conforme se adaptan los ojos aparecen todos los detalles. Al final del pasillo había alguien. una figura. Era una silueta con forma antropomórfica que daba reflejos. Parecia una estatua. Definitivamente era algo similar al mármol negro. Apenas la vi empece a sentir un fuerte olor a frutillas, pero era un olor artificial. Más que parecer el leve aroma que sale de las frutas frescas, parecía incienso de frutillas o aromatizante. En el momento que sentí el olor un escalofrío recorrio mi espalda y me dio un terror increíble. Nunca sentí más miedo en toda mi vida/muerte. Me quede paralizado por un tiempo largo. Por primera vez sentía una presencia ajena a la mía. Venía de la estatua. Me fui acercando con precaución. Llegue a alrededor 10 metros de la estatua. Tenía una altura similar a la mía y definitivamente era algo que reconocí enseguida: era Anubis, el dios egipcio de la muerte. Miles de pensamientos me pasaron
-            por la cabeza. Me desmaye.</p>
+            Antes de poder procesar algún otro pensamiento, en una de las paredes comenzó a aparecer un borrón oscuro del tamaño de una puerta. Era como un agujero negro pero sin un contorno sólido, sino que la oscuridad se desvanecía en un degradado. En segundos, el abismo al otro lado fue tomando forma de a poco, como una foto instantánea revelándose. Claramente, era un agujero que guiaba hacia fuera de la habitación. Me acerqué hacia el portal y pude ver claramente que a pocos pasos del otro lado había un largo pasillo de baldosas de cristal rojo, rodeadas de vacío puro. No dude ni un segundo en salir de la habitación y pararme sobre las baldosas. La transición del piso de la habitación al piso de baldosas me dio tranquilidad, porque apenas pise el suelo del pasillo, note que mis sentidos volvían a su lugar correcto. Sentía mi respiración y el frío de las baldosas. El pasillo se extendía como cien metros, en plena oscuridad, y conforme avanzaba, las baldosas detrás de mí se desvanecían. Al comienzo me asusté porque pensé que se estaban cayendo, pero luego me di cuenta de que si me detenía en un lugar, las baldosas dejaban de desvanecerse. Era como si hubiera un foco de luz siguiéndome al caminar. ¿Son rojas las baldosas o quizás son de color cristal y hay una luz de tono rojizo encima de mí? Sobre mí no había ningún resplandor que confirmase mis pensamientos, pero al mirar hacia arriba un rato, me di cuenta de que se iban formando pequeños puntos brillantes, miles de estrellas. Cuanto más me quedaba mirando al vacío, más detalles aparecían, de la misma manera que los ojos solo ven un par de estrellas cuando salimos de una habitación iluminada hacia afuera y miramos el cielo nocturno y conforme se adaptan los ojos aparecen todos los detalles. Al final del pasillo había alguien. Una figura. Era una silueta con forma antropomórfica que daba reflejos. Parecía una estatua. Definitivamente, era algo similar al mármol negro. Apenas la vi empece a sentir un fuerte olor a frutillas, pero era un olor artificial. Más que parecer el leve aroma que sale de las frutas frescas, parecía incienso de frutillas o aromatizante. En el momento que sentí el olor, un escalofrío recorrió mi espalda y me dio un terror increíble. Nunca sentí más miedo en toda mi vida/muerte. Me quedé paralizado por un tiempo largo. Por primera vez sentía una presencia ajena a la mía. Venía de la estatua. Me fui acercando con precaución. Llegue alrededor 10 metros de la estatua. Tenía una altura similar a la mía y definitivamente era algo que reconocí enseguida: era Anubis, el dios egipcio de la muerte. Miles de pensamientos me pasaron por la cabeza. Me desmayé.
+            </p>
 
             <h2>habitación del tiempo</h2>
             <p style="margin-bottom: 40px;">
-            una escalera de mármol negro completamente cubierta de piedras preciosas.
-            un coral de brillos, tonos metálicos, transparentosos, lechosos,
-            todo tipo de piedras hermosas de todos los colores.
-            comence a subir las escaleras
-            y al llegar a la cima de la escalera apareció otra puerta.
-            salia brillo por los bordes del marco. habia olor a sal.
-            ¿sentia el cuerpo?
+            Una escalera de mármol negro completamente cubierta de piedras preciosas. Un coral de brillos, tonos metálicos, transparentosos, lechosos, todo tipo de piedras hermosas de todos los colores. Comencé a subir las escaleras y al llegar a la cima de la escalera apareció otra puerta. Salía brillo por los bordes del marco. Había olor a sal. ¿Sentía el cuerpo?
             <p/>
 
             <a href="#main">¿dónde mierda estoy?</a>
